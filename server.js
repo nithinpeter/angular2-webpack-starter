1b27bf75d26d8b8567ab7571133b565084ecde23
Nithin Peter <peter@jugglestreet.com>
2016-01-07 16:59:32 +1100

    Add web.config

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
-app.use(express.static(__dirname+"/dist/assets"));
-app.use(express.static(__dirname+"/dist"))
+app.use(express.static(__dirname+"/dist"));
+app.use(express.static(__dirname+"/dist/assets"))
 
 
 
